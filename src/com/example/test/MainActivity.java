0d8bcd757cdf252cca25868926cdfbef27a57605
xlk521 <1012621780@qq.com>
2013-11-08 17:33:32 +0800

    android:表格的画法

---
+++ src/com/example/test/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.view.Menu;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				  WindowManager.LayoutParams.FLAG_FULLSCREEN);//设置全屏
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE |
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);//不弹出软键盘
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		/**
+		  * 设置为横屏
+		  */
+		if(getRequestedOrientation()!=ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
+			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		}
+		super.onResume();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+}
